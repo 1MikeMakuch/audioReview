3250a5b20c9ce23c0feed1872de1b987f42164d8
Mike Makuch <1mikemakuch@gmail.com>
2020-12-31 14:32:26 -0600

    api tests

--- server/src/apis.js
+++ server/src/apis.js
@@ -12,7 +12,6 @@ require('dotenv').config()
 var mysql
 
 function health(req, res) {
-  debug(req.method, req.originalUrl)
   res.status(200).send('Healthy\n')
   res.end()
 }
@@ -34,7 +33,6 @@ async function getComments(req, res) {
   res.json(results)
 }
 async function postComments(req, res) {
-  debug('post', req.params, req.body)
   let userid = Number(req.params.userid)
   let id = req.params.id
   let comments = req.body.comments
@@ -63,17 +61,34 @@ async function postComments(req, res) {
 
   let result
   try {
-    result = await db.comments.post(userid, id, comments)
+    result = await db.comments.create(userid, id, comments)
   } catch (e) {
     debugE(e)
     res.status(400)
   }
-  if (result) {
-    res.status(400)
-  } else {
-    res.status(201)
+
+  res.status(201).end()
+}
+async function delComments(req, res) {
+  let id = req.params.id
+
+  if (!id) {
+    return res.status(404)
   }
-  res.end()
+
+  let results
+
+  try {
+    results = await db.comments.del(id)
+  } catch (e) {
+    debugE(e)
+    res.status(400).end()
+  }
+  if (results.affectedRows) {
+    res.status(200).end()
+  } else {
+    res.status(400).end()
+  }
 }
 async function getKeyVals(req, res) {
   let id = req.params.id
@@ -90,8 +105,9 @@ async function getKeyVals(req, res) {
     debugE(e)
     res.status(404).send(e)
   }
+  if (undefined === result) res.status(404)
 
-  res.json(result)
+  res.send(result)
 }
 async function postKeyVals(req, res) {
   debug('post', req.headers['content-type'], req.params, req.body)
@@ -117,27 +133,45 @@ async function postKeyVals(req, res) {
 
   let result
   try {
-    result = await db.keyvals.post(id, data)
+    result = await db.keyvals.set(id, data)
   } catch (e) {
     debugE(e)
     res.status(400)
   }
   debug('result', result)
-  if (result) {
-    res.status(404)
-  } else {
-    res.status(201)
+  res.status(201).end()
+}
+async function delKeyVals(req, res) {
+  let id = req.params.id
+
+  if (!id) {
+    return res.status(404)
   }
-  res.end()
+
+  let results
+
+  try {
+    results = await db.keyvals.del(id)
+  } catch (e) {
+    debugE(e)
+    res.status(400).end()
+  }
+  if (results.affectedRows) {
+    res.status(200).end()
+  } else {
+    res.status(400).end()
+  }
 }
 function init(app) {
   debug('init')
   app.get('/healthz', health)
   app.get('/comments/:id', getComments)
   app.post('/comments/:id/:userid', postComments)
+  app.delete('/comments/:id', delComments)
   app.get('/keyvals/:id', getKeyVals)
   app.post('/keyvals/:id', postKeyVals)
   app.put('/keyvals/:id', postKeyVals)
+  app.delete('/keyvals/:id', delKeyVals)
 }
 
 module.exports = {init}
---
+++ server/src/test/api.js
@@ -0,0 +1,130 @@
+/* global describe, it, before, after afterEach */
+
+const process = require('process')
+const chai = require('chai')
+const db = require('../db')
+const debug = require('debug')('dt:test:db')
+const debugE = require('debug')('dt:error:db')
+
+chai.use(require('chai-http'))
+
+const expect = chai.expect
+
+const request = chai.request(process.env.TEST_URL)
+
+before('init db', async function() {
+  await db.init()
+})
+
+describe('api', async function() {
+  it('comments', async function() {
+    let userid = 999999999
+    let mp3 = 'test000'
+    let comments = 'this is a test'
+
+    // create a comment
+    let r = await request.post(`/comments/${mp3}/${userid}`).send({comments})
+    expect(r.status).to.equal(201)
+
+    // read it
+    r = await request.get(`/comments/${mp3}`)
+    expect(r.status).to.equal(200)
+    expect(r.body.length).to.be.gt(0)
+    expect(r.body.find(comment => comments === comment.data).data).to.equal(comments)
+    let ids = r.body.map(comment => comment.id)
+
+    // doesn't exist
+    r = await request.get(`/comments/${mp3}+'xyzzy123321'`)
+    expect(r.status).to.equal(200)
+    expect(r.body.length).to.equal(0)
+
+    // delete
+
+    for (var i = 0; i < ids.length; i++) {
+      r = await request.del(`/comments/${ids[i]}`)
+      expect(r.status).to.equal(200)
+    }
+
+    // delete doesn't exist
+    r = await request.del(`/comments/${mp3 + 'testxyzzy123'}'`)
+    expect(r.status).to.equal(400)
+
+    // create errors
+    r = await request.post(`/comments//${userid}`).send({comments})
+    expect(r.status).to.equal(404)
+
+    r = await request.post(`/comments/xxx`).send({comments})
+    expect(r.status).to.equal(404)
+
+    r = await request.post(`/comments`).send({comments})
+    expect(r.status).to.equal(404)
+  })
+
+  it('keyvals', async function() {
+    let key = 'test000000'
+    let val = 'xyzzy111'
+
+    // create
+    let r = await request
+      .post(`/keyvals/${key}`)
+      .set('Content-type', 'text/plain')
+      .send(val)
+    debug('created', key, val)
+    expect(r.status).to.equal(201)
+
+    // read it
+    r = await request.get(`/keyvals/${key}`)
+    expect(r.status).to.equal(200)
+    expect(r.body.val).to.equal(val)
+    debug('read', key, val)
+
+    // set val to an object
+    val = {
+      xyzzy: 'plugh!'
+    }
+    r = await request
+      .post(`/keyvals/${key}`)
+      .set('Content-type', 'application/json')
+      .send(val)
+    expect(r.status).to.equal(201)
+    debug('set to object', key, val)
+
+    // read it
+    r = await request.get(`/keyvals/${key}`)
+    expect(r.status).to.equal(200)
+    expect(r.body.val.xyzzy).to.equal(val.xyzzy)
+    debug('read that', key, val)
+
+    // hmm can't send a number thru chai
+    //val = 123
+    // and yet the string number comes back as a number
+    //     val = '123'
+    //     r = await request
+    //       .post(`/keyvals/${key}`)
+    //       .set('Content-type', 'text/plain')
+    //       .send(val)
+    //     debug('set to number', key, val)
+    //     expect(r.status).to.equal(201)
+    //
+    //     // read it
+    //     r = await request.get(`/keyvals/${key}`)
+    //     debug('r', r.body)
+    //     expect(r.status).to.equal(200)
+    //     expect(r.body.val).to.equal(val)
+
+    // read non existent
+    r = await request.get(`/keyvals/${key + '123123123'}`)
+    expect(r.status).to.equal(404)
+    debug('read non existent')
+
+    // delete
+    r = await request.delete(`/keyvals/${key}`)
+    expect(r.status).to.equal(200)
+    debug('delete keyval created', key)
+
+    // check it's gone
+    r = await request.get(`/keyvals/${key}`)
+    expect(r.status).to.equal(404)
+    debug('check its deleted')
+  })
+})
